b10b40e50f3312ce2b876db8e450650ba4271938
Rocco Barbini <roccobarbi@gmail.com>
2017-04-05 09:37:41 +0200

    Unit tests for Predatore added

---
+++ PredatoreTest.java
@@ -0,0 +1,50 @@
+package predatorieprede01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PredatoreTest {
+	Organismo [] grid = new Organismo[8];
+	
+	@Test
+	public void testReproduce001() {
+		Predatore predatore1 = new Predatore("predatore1", "specie1", 1000, 'X', 0);
+		for(int i = 0; i < 8; i++) grid[i] = null;
+		int spawn = predatore1.reproduce(grid);
+		System.out.println("Spawn result = " + spawn);
+		assertTrue("Preda didn't spawn even if the whole grid was null!", spawn > -1);
+		assertTrue("Preda spawned in an invalid direction", spawn == 1 || spawn == 3 || spawn == 5 || spawn == 7);
+	}
+
+	@Test
+	public void testReproduce002() {
+		Predatore predatore1 = new Predatore("predatore1", "specie1", 1000, 'X', 0);
+		Predatore predatore2 = new Predatore("predatore1", "specie1", 1000, 'X', 0);
+		for(int i = 0; i < 8; i++) grid[i] = predatore2;
+		int spawn = predatore1.reproduce(grid);
+		System.out.println("Spawn result = " + spawn);
+		assertTrue("Preda spawned with full grid", spawn == -1);
+	}
+	
+	@Test
+	public void testReproduce003() {
+		Predatore predatore1 = new Predatore("predatore1", "specie1", 1000, 'X', 0);
+		Predatore predatore2 = new Predatore("predatore1", "specie1", 1000, 'X', 0);
+		for(int i = 0; i < 8; i++) grid[i] = predatore2;
+		grid[5] = null;
+		int spawn = predatore1.reproduce(grid);
+		System.out.println("Spawn result = " + spawn + " with slot 5 = " + (grid[5] == null ? "null" : grid[5]));
+		assertTrue("Preda didn't spawn in the only available slot", spawn == 5);
+	}
+	
+	@Test
+	public void testSetIsAlive() {
+		Predatore predatore1 = new Predatore();
+		predatore1.setIsAlive(false);
+		assertFalse("Predatore should be dead, but it looks alive instead", predatore1.getIsAlive());
+		predatore1.setIsAlive(true);
+		assertFalse("Predatore should stay dead, but it looks like it has been revived", predatore1.getIsAlive());
+	}
+
+}
